9c541edec98a57ecc3e844eaba49127d745837a1
Sophia <78358674+SophiaHAbibi@users.noreply.github.com>
2024-07-29 22:52:02 +0330

    Style (#2)
    * search box
    
    * new style

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,10 +5,14 @@ import MovieList from "./MovieList";
 import ImageCarousel from "./slider";
 
 const Home = () => {
-  return <div className="mt-[50px] w-full px-4 lg:px-[40px] z-[80]">
-     <ImageCarousel />
-    <MovieList />
-  </div>;
+  return (
+    <div className="relative">
+      <ImageCarousel />
+      <div className="mt-[50px] w-full px-4 lg:px-[40px]">
+        <MovieList />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,12 @@
 
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
 import "../index.css";
-import logo from "../assets/images/philogo.png"
+import logo from "../assets/images/philogo.png";
+import { FaUser } from 'react-icons/fa';
 
 const Navbar = () => {
+    const [showMenu, setShowMenu] = useState(false);
   return (
     <nav className="bg-[#193E2C] bg-transparent  transition-all duration-[.3s] top-0 left-0 w-full px-4 lg:px-[40px] z-[80] border-b-4 border-black-500">
         <div className="w-full border-b border-white  py-[20px] flex items-center justify-between transition-all duration-[.3s]">
@@ -14,17 +16,30 @@ const Navbar = () => {
                 </Link>
             </div>
             <div className="hidden lg:flex items-center gap-[12px]">
-                <Link to="/" className="px-[18px] text-[14px] popins hover:bg-[#43AA13] py-[8px] text-[#000] undefined">
+                <Link to="/" className="px-[18px] text-[14px] popins hover:bg-[#fff89c] py-[8px] text-[#000] undefined">
                     Home
                 </Link>
-                <Link to="/MovieList" className="px-[18px] text-[14px] popins hover:bg-[#43AA13] py-[8px] text-[#000] undefined">
+                <Link to="/MovieList" className="px-[18px] text-[14px] popins hover:bg-[#fff89c] py-[8px] text-[#000] undefined">
                     MovieList
                 </Link>
-                <ul>
-                    <li><Link to="/signup">Signup</Link></li>
-                    <li><Link to="/login">Login</Link></li>
-                    <li><Link to="/profile">Profile</Link></li>
-                 </ul>
+                <div className="relative inline-block">
+      <button onClick={() => setShowMenu(!showMenu)} className="focus:outline-none text-xl text-gray-700">
+        <FaUser />
+      </button>
+      {showMenu && (
+        <ul className="absolute right-0 mt-2 w-48 bg-white border border-gray-200 rounded-md shadow-lg z-[800]">
+          <li className="block px-4 py-2 hover:bg-gray-100">
+            <Link to="/signup">Signup</Link>
+          </li>
+          <li className="block px-4 py-2 hover:bg-gray-100">
+            <Link to="/login">Login</Link>
+          </li>
+          <li className="block px-4 py-2 hover:bg-gray-100">
+            <Link to="/profile">Profile</Link>
+          </li>
+        </ul>
+      )}
+    </div>
             </div>
         </div>
 </nav>
